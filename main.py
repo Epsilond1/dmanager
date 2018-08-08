d3b26460cd83d67303561ade9dd4a3e949a69c1a
Semyon Poltorak <epsilond1@yandex.ru>
2018-08-08 00:43:45 +0000

    better

--- main.py
+++ main.py
@@ -35,14 +35,14 @@ class Instance(object):
             try:
                 document.update(load(stream)[self._service_name])
             except YAMLError as exc:
-                logging.error('file reading error. text exceptions: ', exc)
+                print('file reading error. text exceptions: ', exc)
                 raise
 
         broken_params = {k_: 'not found' for k_, v_ in document.items() if not v_}
 
         if broken_params:
-            logging.error('config\'s format is broken')
-            logging.error(broken_params)
+            print('config\'s format is broken')
+            print(broken_params)
             raise AttributeError
 
         self.repo = document['repo']
@@ -51,14 +51,14 @@ class Instance(object):
         self.secrets = document['secrets']
 
     def pull_revision(self):
-        logging.info('try pull')
+        print('try pull')
         g = git.cmd.Git(self.workdir)
         g.pull()
         print('success')
 
     def deploy(self):
         container_id = ''
-        with Popen(['docker' 'ps'], stdout=PIPE) as proc:
+        with Popen(['docker', 'ps'], stdout=PIPE) as proc:
             out = proc.stdout.read().split('\n')
             for line in out:
                 if self.image_name in line:
@@ -67,17 +67,17 @@ class Instance(object):
         if container_id:
             exit_code = call(['docker', 'container', 'stop', container_id])
             if exit_code != 0:
-                logging.error('Vsyo ploxo')
+                print('Vsyo ploxo')
                 raise SystemError
 
         Popen(['docker', 'build', '-t', self.image_name], cwd=self.workdir)
 
         exit_code = call(['docker', 'run', '-d', '-p', '4000:80', self.image_name])
         if exit_code != 0:
-            logging.error('Container does not start')
+            print('Container does not start')
             raise SystemError
 
-        logging.info('Deploy success')
+        print('Deploy success')
 
     def start_worker(self):
         self.pull_revision()
@@ -92,4 +92,4 @@ class Instance(object):
 
 instance = Instance('app')
 instance.load_config()
-# instance.start_worker()
+instance.start_worker()
