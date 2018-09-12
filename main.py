8e7f768c2d7ce5d65fe022dbb85b2ef44163112a
Semyon Poltorak <epsilond1@yandex.ru>
2018-09-12 04:12:06 +0300

    codestyle + add subprocess + net-params move to config

--- main.py
+++ main.py
@@ -10,34 +10,48 @@ from __future__ import unicode_literals
 
 
 import docker
-import git
+import os
 import sys
+from subprocess import Popen
 from yaml import load, YAMLError
 
 
 class Instance(object):
     def __init__(self, service_name):
         self._service_name = service_name
-        self.repo = ''
-        self.workdir = ''
-        self.image_name = ''
-        self.secrets = ''
-        self.command = ''
-        self.client = docker.from_env()
+        self._repo = ''
+        self._workdir = ''
+        self._image_name = ''
+        self._secrets = ''
+        self._client = docker.from_env()
+
+    @staticmethod
+    def run_command(*args, **kwargs):
+        print('start ancestor-process with arguments: {}\n{}'.format(args, kwargs))
+        exit_code = Popen(*args, **kwargs).wait()
+        print('ancestor-process finished with code {}'.format(exit_code))
+        assert exit_code == 0
 
     def load_config(self):
-        document = {
+        must_params = {
             'repo': None,
             'workdir': None,
             'image_name': None,
             'secrets': None,
-            'command': None,
+            'branch': None,
+        }
+
+        other_params = {
+            'port_external': None,
+            'port_internal': None,
+            'protocol': None,
         }
 
         with open('config.yaml') as stream:
             try:
                 try:
-                    document.update(load(stream)[self._service_name])
+                    must_params.update(load(stream)[self._service_name])
+                    other_params.update(load(stream)[self._service_name])
                 except KeyError:
                     print('not found application ', self._service_name)
                     return
@@ -45,29 +59,36 @@ class Instance(object):
                 print('file reading error. text exceptions: ', exc)
                 return
 
-        broken_params = {k_: 'not found' for k_, v_ in document.items() if not v_}
+        broken_params = {k_: 'not found' for k_, v_ in must_params.items() if not v_}
 
         if broken_params:
             print('config\'s format is broken')
             print(broken_params)
             return
 
-        self.repo = document['repo']
-        self.workdir = document['workdir']
-        self.image_name = document['image_name']
-        self.secrets = document['secrets']
-        self.command = document['command']
+        self._repo = must_params['repo']
+        self._workdir = must_params['workdir']
+        if not os.path.exists(self._workdir):
+                self.run_command(['mkdir', '-p', self._workdir])
+        os.chdir(self._workdir) # change root-dir
+        self._image_name = must_params['image_name']
+        self._secrets = must_params['secrets']
+        self._branch = must_params['branch']
+
+        self._port_external = other_params['port_external']
+        self._port_internal = other_params['port_internal']
+        self._protocol = other_params['protocol']
 
     def pull_revision(self):
-        print('try pull')
-        g = git.cmd.Git(self.workdir)
-        g.pull()
-        print('success')
+        self.run_command(['git', 'clone', self._repo, self._workdir])
+        self.run_command(['git', 'checkout', self._branch])
+        self.run_command(['git', 'fetch'])
+        print('git now is actually')
 
     def deploy(self):
         print('search started containers...')
 
-        containers = self.client.containers.list(filters={'status': 'running', 'ancestor': self.image_name + ':latest'})
+        containers = self._client.containers.list(filters={'status': 'running', 'ancestor': self._image_name + ':latest'})
         if len(containers) > 1:
             print('found {} containers... This is problem'.format(len(containers)))
             return
@@ -76,11 +97,13 @@ class Instance(object):
             containers[0].stop()
 
         print('start build image')
-        self.client.images.build(path=self.workdir, tag=self.image_name, dockerfile=self.workdir+'/Dockerfile', pull=True)
+        self._client.images.build(path=self._workdir, tag=self._image_name, dockerfile=self._workdir+'/Dockerfile', pull=True)
         print('building success finished')
 
         print('start build and run container')
-        self.client.containers.run(self.image_name, ports={'80/tcp': 4000}, detach=True)
+        self._client.containers.run(self._image_name, ports={
+            '{}/{}'.format(self._port_external, self._protocol): int(self._port_internal)
+        }, detach=True)
         print('deploy success')
 
     def start_worker(self):
@@ -89,10 +112,10 @@ class Instance(object):
 
     def print_config(self):
         print(self._service_name)
-        print(self.workdir)
-        print(self.image_name)
-        print(self.secrets)
-        print(self.repo)
+        print(self._workdir)
+        print(self._image_name)
+        print(self._secrets)
+        print(self._repo)
 
 
 def __main__():
